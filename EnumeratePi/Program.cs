1082dd2de8c0e95314a63b8739d4e533694301b7
Connor <connormullett@gmail.com>
2018-08-09 14:30:46 -0400

    Initial

--- EnumeratePi/Program.cs
+++ EnumeratePi/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
-namespace EnumeratePi
+namespace PiCalc
 {
 	class Program
 	{
+		readonly BigInteger FOUR = new BigInteger(4);
+		readonly BigInteger SEVEN = new BigInteger(7);
+		readonly BigInteger TEN = new BigInteger(10);
+		readonly BigInteger THREE = new BigInteger(3);
+		readonly BigInteger TWO = new BigInteger(2);
+
+		BigInteger k = BigInteger.One;
+		BigInteger l = new BigInteger(3);
+		BigInteger n = new BigInteger(3);
+		BigInteger q = BigInteger.One;
+		BigInteger r = BigInteger.Zero;
+		BigInteger t = BigInteger.One;
+
+		public void CalcPiDigits()
+		{
+			BigInteger nn, nr;
+			bool first = true;
+			while (true)
+			{
+
+				if ((FOUR * q + r - t).CompareTo(n * t) == -1)
+				{
+					Console.Write(n);
+					if (first)
+					{
+						Console.Write(".");
+						first = false;
+					}
+					nr = TEN * (r - (n * t));
+					n = TEN * (THREE * q + r) / t - (TEN * n);
+					q *= TEN;
+					r = nr;
+				}
+				else
+				{
+					nr = (TWO * q + r) * l;
+					nn = (q * (SEVEN * k) + TWO + r * l) / (t * l);
+					q *= k;
+					t *= l;
+					l += TWO;
+					k += BigInteger.One;
+					n = nn;
+					r = nr;
+				}
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			Program program = new Program();
+			program.CalcPiDigits();
 		}
 	}
 }
